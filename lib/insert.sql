c05288946a0d240d0c22ba52de38e29d6020b9fb
Mark <markjohnguto@gmail.com>
2025-11-24 14:21:40 +0300

    changes

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'Brown', 'Aggressive', 1),
+('Rowdy', 8, 'M', 'Grizzly', 'Playful', 1),
+('Tabitha', 5, 'F', 'Black', 'Calm', 1),
+('Sergeant Brown', 12, 'M', 'Brown', 'Protective', 1),
+('Melissa', 7, 'F', 'Polar', 'Gentle', 1),
+('Grinch', 4, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 6, 'F', 'Panda', 'Curious', 1),
+(NULL, 3, 'F', 'Black', 'Shy', 1);
